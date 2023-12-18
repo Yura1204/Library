987e51fe74c8f6db708aa36a744c7de7ba7d76ce
Yura1204 <yura104bk@gmail.com>
2023-12-19 01:57:15 +0400

    Реалтховано добавление пользователей в БД и аутентификация через них

--- Library/security-service/src/main/java/com/securityservice/securityservice/config/SecurityConfig.java
+++ Library/security-service/src/main/java/com/securityservice/securityservice/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.securityservice.securityservice.config;
 
+import com.securityservice.securityservice.service.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,17 +22,24 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("123")).build();
-        return new InMemoryUserDetailsManager(admin);
+        return (UserDetailsService) new MyUserDetailsService();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/users/**").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/users/**", "/api/users/register").permitAll()
                         .requestMatchers("api/v1/apps/**").authenticated())
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService(passwordEncoder()));
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
     @Bean
